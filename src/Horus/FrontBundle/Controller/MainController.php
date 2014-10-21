b9b4b3ba45ccbd7bb9b64295de000914ad5a13a4
root <you@example.com>
2014-10-21 12:21:04 +0200

    clean FO

---
+++ src/Horus/FrontBundle/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Horus\FrontBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+    /**
+     * Main Page
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        return $this->render('HorusFrontBundle:Main:index.html.twig');
+    }
+}
